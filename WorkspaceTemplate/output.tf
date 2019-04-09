f49de4ef7b3ee1b77c2d1127c5945be6051f0d24
John Studarus <john@jhlconsulting.com>
2019-04-09 17:32:10 +0000

    better output for lab users

--- WorkspaceTemplate/output.tf
+++ WorkspaceTemplate/output.tf
@@ -2,6 +2,10 @@ output "Server IPs v4" {
   value = "${packet_device.hosts.*.access_public_ipv4}"
 }
 
+output "Server SSH Access" {
+  value = "ssh root@${packet_device.hosts.*.access_public_ipv4} -i mykey"
+}
+
 output "Server IPs v6" {
   value = "${packet_device.hosts.*.access_public_ipv6}"
 }
@@ -14,6 +18,6 @@ output "Anycast IPv6 Network" {
   value = "${local.anycast_network}"
 }
 
-output "Test Command" {
+output "IPv6 Anycast Curl Command" {
   value = "curl http://[${local.anycast_addr}]"
 }
