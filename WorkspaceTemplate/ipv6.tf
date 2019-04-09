a1e90d5fada023d49930a35fb632bf9c64fd9476
John Studarus <john@jhlconsulting.com>
2019-04-09 17:31:57 +0000

    better subnetting across lab environments

--- WorkspaceTemplate/ipv6.tf
+++ WorkspaceTemplate/ipv6.tf
@@ -6,6 +6,7 @@ data "packet_precreated_ip_block" "ipv6" {
 }
 
 locals {
-  anycast_network   = "${cidrsubnet(data.packet_precreated_ip_block.ipv6.cidr_notation,8,2)}"
-  anycast_addr      = "${cidrhost(local.anycast_network,var.lab_number)}"
+  # 8 bits is 2^8 or 256 labs
+  anycast_network   = "${cidrsubnet(data.packet_precreated_ip_block.ipv6.cidr_notation,8,var.lab_number)}"
+  anycast_addr      = "${cidrhost(local.anycast_network,1)}"
 }
