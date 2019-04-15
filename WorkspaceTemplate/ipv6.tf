2dca717091a180a48c84d2a308551e06767fa89c
John Studarus <john@jhlconsulting.com>
2019-04-15 20:56:04 +0000

    addr_1 but not network_1y

--- WorkspaceTemplate/ipv6.tf
+++ WorkspaceTemplate/ipv6.tf
@@ -8,5 +8,5 @@ data "packet_precreated_ip_block" "ipv6" {
 locals {
   # 8 bits is 2^8 or 256 labs
   anycast_network   = "${cidrsubnet(data.packet_precreated_ip_block.ipv6.cidr_notation,8,var.lab_number)}"
-  anycast_addr_1    = "${cidrhost(local.anycast_network_1,1)}"
+  anycast_addr_1    = "${cidrhost(local.anycast_network,1)}"
 }
