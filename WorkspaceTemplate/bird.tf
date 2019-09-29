11b0717e575559d6240347444c0dcdbe1345eede
John Studarus <john@jhlconsulting.com>
2019-09-29 18:51:19 +0000

    bird wasn't installing

--- WorkspaceTemplate/bird.tf
+++ WorkspaceTemplate/bird.tf
@@ -1,6 +1,8 @@
 
 resource "null_resource" "bird" {
 
+    depends_on = ["packet_device.hosts"]
+
     count = "${var.instance_count}"
 
     connection {
@@ -12,7 +14,8 @@ resource "null_resource" "bird" {
 
     provisioner "remote-exec" {
         inline = [
-            "apt-get -y install bird > apt-get-bird-out.txt",
+            "apt-get -y update > apt-get-update-out.txt",
+            "apt-get -y install bird > apt-get-bird-out.txt 2> apt-get-gird-err.txt",
             "sysctl net.ipv4.ip_forward=1",
             "sysctl net.ipv6.conf.all.forwarding=1",
         ]
